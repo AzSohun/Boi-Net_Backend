9ea87f75d036579bda86a199eb129f8993aaa6fc
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-04 22:09:40 +0600

    Fixed the InvalidCastingException by EF Core Fluent API Configuration

--- Boi.Net/Data/BoiNetDbContext.cs
+++ Boi.Net/Data/BoiNetDbContext.cs
@@ -13,5 +13,24 @@ namespace Boi.Net.Data
 
         public DbSet<Book> Books { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            // ==========================================================
+            // EF Core Fluent API Configurations
+            // ==========================================================
+
+            // Price কলামটিকে ডাটাবেজে decimal(18,2) হিসেবে ফোর্স করা হচ্ছে।
+            // কারণ: Double টাইপ ব্যবহার করলে Precision Loss এবং InvalidCastException হতে পারে।
+            modelBuilder.Entity<Book>()
+                .Property(book => book.Price)
+                .HasColumnType("decimal(18, 2)");
+        }
+
     }
+
+
 }
