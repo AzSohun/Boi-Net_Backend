84b78b270cc0c7cb0f6c4ddfe05925c08b67cde7
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-09 01:06:53 +0600

    Added: All the User and Auth Dtos

---
+++ Boi.Net/DTOs/AuthDTOs/RegistrationDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boi.Net.DTOs.AuthDTOs
+{
+    public class RegistrationDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(30, ErrorMessage = "Name cannot exceed 30 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        public string PasswordHash { get; set; } = string.Empty;
+
+    }
+}
---
+++ Boi.Net/DTOs/UserDTOs/UpdateUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boi.Net.DTOs.UserDTOs
+{
+    public class UpdateUserDto
+    {
+        [MaxLength(30, ErrorMessage = "Name cannot exceed 30 characters")]
+        public string? Name { get; set; }
+
+        public DateOnly? DOB { get; set; }
+
+        // ফাইলের জন্য সঠিক নাম
+        public IFormFile? ProfilePhoto { get; set; }
+    }
+}
