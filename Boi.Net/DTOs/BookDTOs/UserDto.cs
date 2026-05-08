d000822193b91689310e7fafdadab68900a94486
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-09 00:47:45 +0600

    Added: UserDto

--- Boi.Net/DTOs/BookDTOs/UserDto.cs
+++ Boi.Net/DTOs/BookDTOs/UserDto.cs
@@ -1,6 +1,25 @@
-﻿namespace Boi.Net.DTOs.BookDTOs
+﻿using Boi.Net.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boi.Net.DTOs.BookDTOs
 {
     public class UserDto
     {
+        public Guid id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+        
+        public string Name { get; set; } = string.Empty;
+
+        public string ProfilePhotoUrl { get; set; } = string.Empty;
+
+        public string ProfilePhotoId { get; set; } = string.Empty;
+
+        public DateOnly? DOB { get; set; }
+
+        public Role UserRole { get; set; } = Role.User;
+
+        public ICollection<BookDto> Wishlist { get; set; } = new List<BookDto>();
+
     }
 }
