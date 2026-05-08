fa9ebe74bf051d8d554c29ef1aa7575751ca2dd2
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-09 00:57:00 +0600

    Added: User Profile Response Dto

---
+++ Boi.Net/DTOs/AuthDTOs/UserProfileResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using Boi.Net.DTOs.BookDTOs;
+
+namespace Boi.Net.DTOs.AuthDTOs
+{
+    public class UserProfileResponseDto
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string ProfilePhotoUrl { get; set; } = string.Empty;
+
+        public DateOnly? DOB { get; set; }
+
+        public Role UserRole { get; set; } = Role.User;
+
+        public ICollection<BookDto> Wishlist { get; set; } = new List<BookDto>();
+    }
+}
