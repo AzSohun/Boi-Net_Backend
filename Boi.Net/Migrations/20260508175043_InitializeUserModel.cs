e740601b61892fb74652dc30a3b90305811fbd9f
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-08 23:51:45 +0600

    Added: User Nodel

---
+++ Boi.Net/Migrations/20260508175043_InitializeUserModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Boi.Net.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Boi.Net/Model/User.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Boi.Net.Model
+{
+    public class User
+    {
+
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; } = string.Empty;
+
+        public string ProfilePhotoUrl { get; set; } = string.Empty;
+
+        public string ProfilePhotoId { get; set; } = string.Empty;
+
+        public DateOnly? DOB { get; set; }
+
+        public Role UserRole { get; set; } = Role.User;
+
+        // EF Core doen't support array directly in the database.
+        // Here ICollection is the solution of create Many-to-Many relational table.
+        public ICollection<Book> Wishlist { get; set; } = new List<Book>();
+
+        // TODO:
+        // public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+        public bool IsBlocked { get; set; } = false;
+
+        public bool IsDeleted { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+
+    public enum Role
+    {
+        SuperAdmin,
+        Admin,
+        User
+    }
+
+
+}
