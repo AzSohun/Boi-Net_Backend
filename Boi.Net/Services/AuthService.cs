76663f57b14b5cd2c401c95aad11c7aec7c51f5d
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-09 12:30:23 +0600

    Auth Service Added

--- Boi.Net/Services/AuthService.cs
+++ Boi.Net/Services/AuthService.cs
@@ -1,4 +1,7 @@
 ﻿using Boi.Net.Data;
+using Boi.Net.DTOs.AuthDTOs;
+using Boi.Net.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Boi.Net.Services
 {
@@ -14,10 +17,32 @@ namespace Boi.Net.Services
 
 
         // RegisterUser
-        public async Task Register(string Name, string Email, string Password)
+        public async Task<bool> Register(string Name, string Email, string Password)
         {
 
+            var user = await _context.Users.FirstOrDefaultAsync(user => user.Email == Email);
 
+            if(user != null)
+            {
+                return false;
+            }
+            if(user == null && _context.Users.ToListAsync() == null)
+            {
+                user?.UserRole = Role.SuperAdmin;
+            }
+
+            var PasswordHash = BCrypt.Net.BCrypt.HashPassword(Password);
+
+            var newUser = new RegistrationDto
+            {
+                Email = user?.Email!,
+                Name = user?.Name!,
+                Password = Password
+            };
+
+            await _context.AddAsync(newUser);
+
+            return true;
 
         }
 
