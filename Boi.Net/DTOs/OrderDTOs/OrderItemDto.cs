42bbeb231725f218b19566a3f76129897a2ff495
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-12 01:07:59 +0600

    Added OrderItemDto

---
+++ Boi.Net/DTOs/OrderDTOs/OrderItemDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Boi.Net.DTOs.OrderDTOs
+{
+
+
+    public class CreateOrderDto
+    {
+        public List<OrderItemDto> Items { get; set; } = new List<OrderItemDto>();
+    }
+
+
+    public class OrderItemDto
+    {
+        public int BookId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
