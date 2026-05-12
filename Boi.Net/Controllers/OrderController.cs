c32e13adf5106ca3b53d597e1a2ddef0329ca279
Asaduzzaman Sohun <148855210+code-with-sohun@users.noreply.github.com>
2026-05-12 15:33:56 +0600

    Added: Order Controller

---
+++ Boi.Net/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Boi.Net.DTOs.OrderDTOs;
+using Boi.Net.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Boi.Net.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+        private readonly OrderService _service;
+
+        public OrderController(OrderService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("place-order")]
+        public async Task<ActionResult> PlaceeOrder([FromBody] CreateOrderDto createOrderDto)
+        {
+
+            var userId =  User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var order = await _service.CreateOrderAsync(userId!, createOrderDto);
+
+            return Ok(new {
+            Message= "Order Placed Successfully",
+            Orderid = order.Id,
+            TotalAmount = order.TotalAmount
+            });
+
+        }
+    }
+}
